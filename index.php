16a719b3da40d583f883049ce027c06042d57179
Mark Horton <mark.horton@totallyboundless.com>
2015-08-04 01:57:44 +0100

    Changes to allow for 3 sites.

--- index.php
+++ index.php
@@ -1,13 +1,48 @@
+<?php
+switch ($_SERVER['SERVER_NAME']) {
+    case "www.classicalpine.com":
+        $title = "Classic Alpine Ski Holidays";
+        $subtitle = "Hassle free, care free, high quality holidays for people aged 55 and over that have a love of the mountains and particularly skiing.";
+        $css = "classic.css";
+        $logo = "classic";
+        $list = "http://newsletters.boundlessmarketing.co.uk/t/j/s/xkhthy/";
+        $field = "cm-xkhthy-xkhthy";
+        $brands_title = "Our Resorts";
+        break;
+    case "www.vividmountain.com":
+        $title = "Vivid Mountain Ski Holidays";
+        $subtitle = "Hassle free, care free, high quality holidays for gay men aged 35 and over that have a love of the mountains and particularly skiing.";
+        $css = "vivid.css";
+        $logo = "vivid";
+        $list = "http://newsletters.boundlessmarketing.co.uk/t/j/s/xkhthj/";
+        $field = "cm-xkhthj-xkhthj";
+        $brands_title = "Our Resorts";
+        break;
+    default:
+        $title = "The Mountain Experience Company";
+        $subtitle = "Niche holiday brands in a mountain environment.";
+        $css = "main.css";
+        $logo = "mountain";
+        $list = "http://newsletters.boundlessmarketing.co.uk/t/j/s/xkhtll/";
+        $field = "cm-xkhtll-xkhtll";
+        $brands_title = "Our Brands";
+        $brands = array(
+            1 => array("name" => "CLASSIC ALPINE", "url" => "http://www.classicalpine.com/"),
+            2 => array("name" => "VIVID MOUNTAIN", "url" => "http://www.vividmountain.com/")
+        );
+        break;
+}
+?>
 <!doctype html>
 <html class="no-js" lang="EN">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Mountain Experience: is coming soon&hellip;</title>
-        <meta name="description" content="">
+        <title><?php echo $title; ?>: is coming soon&hellip;</title>
+        <meta name="description" content="<?php echo $subtitle; ?>">
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/css/<?php echo $css; ?>">
         <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
         
         <!--[if IE 7]><link rel="stylesheet" href="assets/css/fontello/fontello-ie7.min.css"><![endif]-->
@@ -32,25 +67,30 @@
         <div class="container">
         <section id="main">
             <header>
-                <img src="assets/img/mountain_logo.png" alt="Mountain Experience" width="315" height="181">
+                <img src="assets/img/<?php echo $logo; ?>_logo.png" alt="Mountain Experience" width="315" height="181">
             </header>
-            <p>The Mountain Experience Co. is coming soon &hellip; Niche holiday brands in a mountain environment.</p>
+            <p><?php echo $title; ?> is coming soon &hellip; <?php echo $subtitle; ?></p>
             <footer>
                 <p>For more information enter your email address below</p>
-                <form action="http://newsletters.boundlessmarketing.co.uk/t/j/s/xkhtll/" method="post" id="subForm">
+                <form action="<?php echo $list; ?>" method="post" id="subForm">
                     <div class="input-group">
-                      <input type="email" id="fieldEmail" name="cm-xkhtll-xkhtll" class="form-control" required placeholder="Enter your email address">
+                      <input type="email" id="fieldEmail" name="<?php echo $field; ?>" class="form-control" required placeholder="Enter your email address">
                       <span class="input-group-btn">
                         <button class="btn btn-default" type="submit">Subscribe</button>
                       </span>
                     </div>
                 </form>
                 <div>
-                <h2><span></span>Our Brands<span></span></h2>
+                <h2><span></span><?php echo $brands_title; ?><span></span></h2>
                 <div></div>
                 <ul>
-                    <li>CLASSIC ALPINE</li>
-                    <li>VIVID MOUNTAIN</li>
+                    <?php foreach($brands AS $brand) { ?><span></span></h2>
+                      <?php if ($brand['url'] == "") { ?>
+                        <li><?php echo $brand['name']; ?></li>
+                      <?php } else { ?>
+                        <li><a href="<?php echo $brand['url']; ?>" title="<?php echo $brand['name']; ?>"><?php echo $brand['name']; ?></a></li>
+                      <?php } ?>
+                    <?php } ?><span></span></h2>
                 </ul>
                 </div>
             </footer>
